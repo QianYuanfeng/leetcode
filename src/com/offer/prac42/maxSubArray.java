2ac18a9f76d732910252101fc93112299457226c
Qianyuanfeng <yuanfeng.qian@hand-china.com>
2022-07-14 11:18:01 +0800

    【动态规划】剑指 Offer 42. 连续子数组的最大和 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。 要求时间复杂度为O(n)。

---
+++ src/com/offer/prac42/maxSubArray.java
@@ -0,0 +1,32 @@
+package com.offer.prac42;
+
+import java.util.Arrays;
+
+/**
+ * 剑指 Offer 42. 连续子数组的最大和
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * <p>
+ * 要求时间复杂度为O(n)。
+ */
+public class maxSubArray {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int i = maxSubArray(nums);
+        System.out.println(i);
+    }
+
+    public static int maxSubArray(int[] nums) {
+
+        int[] dp = new int[nums.length + 1];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+
+}
