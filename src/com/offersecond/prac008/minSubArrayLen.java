e9c005c10511bf203030c265639a6618893df109
yvonne <qianyuanf@163.com>
2022-08-24 20:55:46 +0800

    【前缀和】【中等】剑指 Offer II 008. 和大于等于 target 的最短子数组  * 给定一个含有n个正整数的数组和一个正整数 target 。  * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。  *

---
+++ src/com/offersecond/prac008/minSubArrayLen.java
@@ -0,0 +1,33 @@
+package com.offersecond.prac008;
+
+/**
+ * 剑指 Offer II 008. 和大于等于 target 的最短子数组
+ * 给定一个含有n个正整数的数组和一个正整数 target 。
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *
+ * 
+ */
+public class minSubArrayLen {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+
+    }
+
+}
