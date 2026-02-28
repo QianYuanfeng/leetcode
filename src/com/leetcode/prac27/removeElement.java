4f49809fc2ed79bf0b8e39b06e8d5b1915d9394a
Qianyuanfeng <yuanfeng.qian@hand-china.com>
2026-02-28 09:55:09 +0800

    【数组】【简单】27. 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
                假设 nums 中不等于 val 的元素数量为 k，要通过此题，您需要执行以下操作：
    
                更改 nums 数组，使 nums 的前 k 个元素包含不等于 val 的元素。nums 的其余元素和 nums 的大小并不重要。

--- src/com/leetcode/prac27/removeElement.java
+++ src/com/leetcode/prac27/removeElement.java
@@ -5,22 +5,23 @@ import java.util.Arrays;
 /**
  * 27. 移除元素
  * 给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
- *
+ * <p>
  * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
- *
+ * <p>
  * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode.cn/problems/remove-element
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class removeElement {
     public static void main(String[] args) {
-        int[] nums = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
-        int val = 2;
-        int i = removeElement(nums, val);
+        int[] nums = new int[]{3, 2, 2, 3};
+        int val = 3;
+        int i = removeElement20260228(nums, val);
         System.out.println(i);
     }
+
 
     public static int removeElement(int[] nums, int val) {
         int leftIndex = 0;
@@ -38,6 +39,23 @@ public class removeElement {
 
 
         return leftIndex;
+
+    }
+
+
+    public static int removeElement20260228(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        for (int i = k ; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        //System.out.println(Arrays.toString(nums));
+        return k;
 
     }
 
